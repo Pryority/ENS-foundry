9215bcfed6e6f41fd21e8c8884e1805b5601ac4f
pryority <matthewapryor@gmail.com>
2023-04-30 23:48:37 -0400

    base registrar now can setApprovalForAll, need to set resolver?

--- test/BaseRegistrarImplementation.t.sol
+++ test/BaseRegistrarImplementation.t.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.19;
 import "ds-test/test.sol";
 import "@openzeppelin/token/ERC721/IERC721.sol";
 import "../src/BaseRegistrarImplementation.sol";
+import "../src/resolvers/Resolver.sol";
 import "./mocks/MockENSRegistry.sol";
+import "./mocks/MockResolver.sol";
+import { Vm } from "forge-std/Vm.sol";
+import "../src/registry/ENS.sol";
 
 interface TestRegistrar {
     function ownerOf(uint256 tokenId) external view returns (address);
@@ -69,56 +73,59 @@ contract BaseRegistrarTest is DSTest {
     TestBaseRegistrar public testBaseRegistrar;
     BaseRegistrarImplementation public baseRegistrar;
     MockENSRegistry public mockENS;
-    TestENS public ens;
+    Resolver public resolver;
+    ENS public ens;
     IERC721 public token;
+    Vm vm;
     bytes32 public node;
     bytes32 public parentNode;
     bytes32 public baseNode = 0x2e65746800000000000000000000000000000000000000000000000000000000;
+    ENS public EnsContract = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
     uint256 constant duration = 31536000; // 1 year
 
     function setUp() public {
-        registrar = TestRegistrar(address(this));
-        baseRegistrar = new BaseRegistrarImplementation(mockENS, baseNode);
+        registrar = TestRegistrar(address(1));
+        mockENS = new MockENSRegistry();
+        testBaseRegistrar = TestBaseRegistrar(address(new BaseRegistrarImplementation(mockENS, keccak256(abi.encodePacked("eth")))));
+        baseRegistrar = BaseRegistrarImplementation(address(testBaseRegistrar));
         token = IERC721(address(baseRegistrar));
-        ens = TestENS(address(this));
+        ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e); // Mainnet ENS address
         node = keccak256(abi.encodePacked("test"));
         parentNode = keccak256(abi.encodePacked("eth"));
-        ens.setApprovalForAll(address(baseRegistrar), true);
-        ens.setResolver(parentNode, address(this));
-        ens.setResolver(node, address(this));
+        baseRegistrar.setApprovalForAll(address(mockENS), true);
+        // baseRegistrar.setResolver(address(resolver));
+        // baseRegistrar.setResolver(bytes32(parentNode));
     }
-
-    function test_mint() public {
-        uint256 price = testBaseRegistrar.rentPrice("test-lala.eth", duration);
-        token.approve(address(baseRegistrar), price);
-        uint256 balanceBefore = token.balanceOf(address(this));
-        testBaseRegistrar.mint(uint256(node), address(this), duration);
-        uint256 balanceAfter = token.balanceOf(address(this));
-        assertEq(balanceBefore + 1, balanceAfter);
-        assertEq(registrar.ownerOf(uint256(node)), address(this));
-    }
-
-    function test_mint_not_available() public {
-        testBaseRegistrar.mint(uint256(node), address(this), duration);
-        token.approve(
-            address(baseRegistrar),
-            testBaseRegistrar.rentPrice("test-lala.eth", duration)
-        );
-        baseRegistrar.renew(uint256(node), duration);
-        assert(baseRegistrar.available(uint256(node)) == false);
-        uint256 balanceBefore = token.balanceOf(address(this));
-        (bool success, ) = address(baseRegistrar).call(
-            abi.encodeWithSelector(
-                testBaseRegistrar.mint.selector,
-                uint256(node),
-                address(this),
-                duration
-            )
-        );
-        assert(success == false);
-        uint256 balanceAfter = token.balanceOf(address(this));
-        assertEq(balanceBefore, balanceAfter);
-        // assertEq(registrar.ownerOf(uint
-    }
+    
+//    function test_register() public {
+//         bytes32 labelHash = keccak256(abi.encodePacked("test-name"));
+//         baseRegistrar.register(uint256(labelHash), address(this), duration);
+//     }
 }
+
+
+
+
+    // function test_mint_not_available() public {
+    //     testBaseRegistrar.mint(uint256(node), address(this), duration);
+    //     token.approve(
+    //         address(baseRegistrar),
+    //         testBaseRegistrar.rentPrice("test-lala.eth", duration)
+    //     );
+    //     baseRegistrar.renew(uint256(node), duration);
+    //     assert(baseRegistrar.available(uint256(node)) == false);
+    //     uint256 balanceBefore = token.balanceOf(address(this));
+    //     (bool success, ) = address(baseRegistrar).call(
+    //         abi.encodeWithSelector(
+    //             testBaseRegistrar.mint.selector,
+    //             uint256(node),
+    //             address(this),
+    //             duration
+    //         )
+    //     );
+    //     assert(success == false);
+    //     uint256 balanceAfter = token.balanceOf(address(this));
+    //     assertEq(balanceBefore, balanceAfter);
+    //     // assertEq(registrar.ownerOf(uint
+    // }
